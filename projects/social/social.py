e52b4c374737c9df57bf3333688cc637861001fd
Pinefoot <jameslcarpino@gmail.com>
2020-07-15 17:46:28 -0700

    working breadth first search for social paths. cna't get all of the key:values yet though. working on the loop

--- projects/social/social.py
+++ projects/social/social.py
@@ -1,6 +1,11 @@
+import random
+from util import Queue
+
+
 class User:
     def __init__(self, name):
         self.name = name
+
 
 class SocialGraph:
     def __init__(self):
@@ -14,7 +19,10 @@ class SocialGraph:
         """
         if user_id == friend_id:
             print("WARNING: You cannot be friends with yourself")
-        elif friend_id in self.friendships[user_id] or user_id in self.friendships[friend_id]:
+        elif (
+            friend_id in self.friendships[user_id]
+            or user_id in self.friendships[friend_id]
+        ):
             print("WARNING: Friendship already exists")
         else:
             self.friendships[user_id].add(friend_id)
@@ -45,8 +53,24 @@ class SocialGraph:
         # !!!! IMPLEMENT ME
 
         # Add users
+        for i in range(num_users):
+            self.add_user(f"User {1}")
 
         # Create friendships
+        possible_friendships = []
+        for user_id in self.users:
+            for friend_id in range(user_id + 1, self.last_id + 1):
+                possible_friendships.append((user_id, friend_id))
+
+        # shuffle possible friends
+        random.shuffle(possible_friendships)
+
+        # add friendships
+        for i in range(num_users * avg_friendships // 2):
+            friendships = possible_friendships[i]
+            self.add_friendship(friendships[0], friendships[1])
+
+        # number of users must be greater than the average number of friendships
 
     def get_all_social_paths(self, user_id):
         """
@@ -59,12 +83,69 @@ class SocialGraph:
         """
         visited = {}  # Note that this is a dictionary, not a set
         # !!!! IMPLEMENT ME
+        # create that empty queue you hear so much about
+        def friendly_paths(path, friends):
+            q = Queue()
+            q.enqueue([user_id])
+            friends = []
+            while q.size() > 0:
+                path = q.dequeue()
+                last_vertex = path[-1]
+                if last_vertex not in visited:
+                    for neighbor in self.friendships[user_id]:
+                        path_copy = list(path)
+                        path_copy.append(neighbor)
+                        if neighbor == last_vertex:
+                            return path_copy
+
+                    visited[user_id] = path
+
+        for friends in self.users:
+            results = friendly_paths(user_id, [friends])
+            if results != None:
+                visited[friends] = results
+
         return visited
 
+    """
+    #go through every other element in the graph,
+    for all_the_friends in self.users[user_id]:
+    #  check if it's related to your starting element,
+        if all_the_friends == user_id:
+            return all_the_friends
+    #  and if it is, add the path to the dictionary
+                visited[user_id] = path
+    """
 
-if __name__ == '__main__':
+    """
+        # UNDERSTAND:
+        # the first user: show the stops along the path to get to that friend
+        # the next users are based on the next level of friends of the first user
+        # if user 1 has friends 8, 10, 5
+        # the next users that are explored are those that are within the first degree of seperation of the user 1.
+        # -so whatever the path of going from 1 -> the second degree seperated. ie: if userid = 1 -> 8 -> read the friends of 8, got 2 -> 2:{1, 8, 2}
+        # then all of that information once they've been recorded and the friend list exhausted, returns the whole dictionary of that.
+
+        # first need to set up a bfs
+        # need a queue
+        # need a way to keep track of the paths
+        # putthe paths into the dictonary
+
+        # PLAN:
+        # bfs to find the paths of all users/friends store them in the dic.
+        #  if the user's id has no friends(neighbors) return the path as its id
+        # if the user's id has friends:
+        # store the friends' ID
+        # put the friend into the array to be returned with its path to the original user
+        #
+        # Returns a dictionary containing every user in that user's
+        # extended network with the shortest friendship path between them.
+"""
+
+
+if __name__ == "__main__":
     sg = SocialGraph()
     sg.populate_graph(10, 2)
-    print(sg.friendships)
+    print(f"List of Friendships{sg.friendships}")
     connections = sg.get_all_social_paths(1)
-    print(connections)
+    print(f"Connector paths: {connections}")
---
+++ projects/social/socialnotes.py
@@ -0,0 +1,34 @@
+"""
+~Notes and Ideas~
+
+
+Understand:
+    --implement a function that shows all the friends in a users extended network that link them--
+
+    dictionary key: particular user
+    
+    dictionary value: 
+    direct friend
+
+
+understanding the relationships:
+    if 1 is a direct friend of 5, 2 is a friend of a friend.
+
+    because 2 is friends with 5, but not directly friends with one
+
+
+
+
+
+Plan:
+
+Program:
+
+Evaluate:
+
+Review:
+
+
+
+
+"""
